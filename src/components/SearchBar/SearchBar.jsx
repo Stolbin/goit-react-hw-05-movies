55a2f7eea8b32174fe3829511d6bbdba61084848
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-03-23 19:39:42 +0200

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { SearchForm, FormButton, FormInput, Icon } from "./SearchBar.styled";
 import PropTypes from "prop-types";
 
-export function SearchBar({ onSubmit }) {
+export const SearchBar = ({ onSubmit }) => {
   const [query, setQuery] = useState("");
 
   const handleChange = (e) => {
@@ -29,6 +29,6 @@ export function SearchBar({ onSubmit }) {
       </FormButton>
     </SearchForm>
   );
-}
+};
 
 SearchBar.propTypes = { onSubmit: PropTypes.func.isRequired };
