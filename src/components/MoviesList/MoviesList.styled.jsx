9ae3f2b9712c3a120f270c5096479cff0e03d8a4
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-04-10 22:43:58 +0300

    Update MoviesList.styled.jsx

--- src/components/MoviesList/MoviesList.styled.jsx
+++ src/components/MoviesList/MoviesList.styled.jsx
@@ -14,10 +14,10 @@ export const MoviesCard = styled.div`
   transform: scale(1);
   box-shadow: 3px 3px 5px #696969;
 
-    transition: transform 450ms cubic-bezier(0, 0.110, 0.35, 2);
+    transition: transform 450ms linear (0, 0.110, 0.35, 2);
   &:hover {
     transform: scale(1.02);
-    transition: transform  450ms cubic-bezier(0, 0.110, 0.35, 2);
+    transition: transform  450ms linear (0, 0.110, 0.35, 2);
   }
 `;
 
