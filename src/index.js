40edd1ab4fbeffd3cd4d4506a9111adaa62a4c81
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-04-10 20:29:33 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "components/App";
+import App from "App";
 import "./index.css";
 import { Global } from "@emotion/react";
 import { GlobalStyles } from "./Styles/GlobalStyles.jsx";
